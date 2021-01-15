988897cfebc529dc50678a9aff31687baa51ddcf
Grant Colestock <grant@colestock.net>
2021-01-14 19:33:32 -0600

    fix: type definition

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 type testFn = (name: string, fn: () => void | Promise<void>) => void
 
-export declare function describe(headline: string, fn: (args: {
+export default function describe(headline: string, fn: (args: {
   test: testFn & { only: testFn, skip: testFn }
   it: testFn & { only: testFn, skip: testFn }
   beforeAll: (fn: () => void | Promise<void>) => void
